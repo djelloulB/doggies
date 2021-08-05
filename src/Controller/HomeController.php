b76de0eee33d73b47e7dbc2dcfd059531eabdaad
dbcrea <djelloul.be@gmail.com>
2021-08-05 13:56:32 +0200

    affichage des données sur la page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,50 +11,56 @@ use App\Entity\Dog;
 use App\Entity\Image;
 use App\Entity\Annonce;
 use App\Entity\Annonceur;
+use App\Repository\AnnonceRepository;
+use App\Repository\AnnonceurRepository;
+use App\Repository\DogRepository;
+use App\Repository\ImageRepository;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home_index")
      */
-    public function index(): Response
+    public function index(AnnonceurRepository $annonceurRepository, AnnonceRepository $annonceRepository): Response
     {
-        
-        $dogs = [];
-        
-        for($i= 0 ; $i <=5 ; $i++){
+        $annonces = $annonceRepository->findAll();
+        $annonceurs = $annonceurRepository->findAll();
 
-            $dog = new Dog();
-            $dog->setNom('Arturo');
-            $dog->setIfLof(1);
-            $dog->setAntecedents('Arturo est un ancien chien de cirque. ');
-            $image = new Image();
-            $image->setUrlImage('img/Arturo/arthuro.jpg');
-            $dog->addImage($image);
+                // $dogs = [];
+        
+        // for($i= 0 ; $i <=5 ; $i++){
+
+        //     $dog = new Dog();
+        //     $dog->setNom('Arturo');
+        //     $dog->setIfLof(1);
+        //     $dog->setAntecedents('Arturo est un ancien chien de cirque. ');
+        //     $image = new Image();
+        //     $image->setUrlImage('img/Arturo/arthuro.jpg');
+        //     $dog->addImage($image);
             
-            $dogs[] = $dog;
-        }
-        $Annonces = [];
-        for($i= 0 ; $i <5 ; $i++){
-            $annonce = new Annonce();
-            $annonce->setTitre('Arturo');
-            $annonce->setDateMAJ(new \DateTime());
-            $annonce->addDog($dogs[$i]);
-            $Annonces[] = $annonce;
-        }
-        $Annonceurs = [];
-        $annonceur = new Annonceur();
-        $annonceur->setNom('Jean');
-        $annonceur->setPrenom('Michel');
-        $annonceur->setMotDePasse('123');
-        $annonceur->setEmail('jm@fr.fr');
-        $annonceur->setTelephone('0620103000');
-        $Annonceurs[] = $annonceur;
+        //     $dogs[] = $dog;
+        // }
+        // $Annonces = [];
+        // for($i= 0 ; $i <5 ; $i++){
+        //     $annonce = new Annonce();
+        //     $annonce->setTitre('Arturo');
+        //     $annonce->setDateMAJ(new \DateTime());
+        //     $annonce->addDog($dogs[$i]);
+        //     $Annonces[] = $annonce;
+        // }
+        // $Annonceurs = [];
+        // $annonceur = new Annonceur();
+        // $annonceur->setNom('Jean');
+        // $annonceur->setPrenom('Michel');
+        // $annonceur->setMotDePasse('123');
+        // $annonceur->setEmail('jm@fr.fr');
+        // $annonceur->setTelephone('0620103000');
+        // $Annonceurs[] = $annonceur;
 
         return $this->render('home/index.html.twig', [
-            'title' => 'Dog Corner',
-            'annonces' => $Annonces,
-            'annonceurs' => $Annonceurs,
+            
+            'annonces' => $annonces,
+            'annonceurs' => $annonceurs,
         ]);
     }
 }
