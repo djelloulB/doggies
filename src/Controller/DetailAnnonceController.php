af7122e556750e9320035367f4b03f31c9f8e7b9
YDerieux <ydhbcda@gmail.com>
2021-08-06 16:37:14 +0200

    44_DetailAnnonce_Dynamique

--- src/Controller/DetailAnnonceController.php
+++ src/Controller/DetailAnnonceController.php
@@ -9,43 +9,45 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Dog;
 use App\Entity\Image;
 use App\Entity\Annonce;
+use App\Repository\AnnonceRepository;
+use App\Repository\AnnonceurRepository;
+use Doctrine\ORM\Mapping\Id;
 
 class DetailAnnonceController extends AbstractController
 {
     /**
-     * @Route("/detailAnnonce", name="detailAnnonce")
+     * @Route("/annonce/{id}", name="detailAnnonce")
      */
-    public function index(): Response
+    public function index(Annonce $annonce): Response
     {
+        $annonceur = $annonce->getAnnonceur();
 
-
-        $dogs = [];
+        // $dogs = [];
         
-        for($i= 0 ; $i <=1 ; $i++){
+        // for($i= 0 ; $i <=1 ; $i++){
 
-            $dog = new Dog();
-            $dog->setNom('Arturo');
-            $dog->setIfLof(1);
-            $dog->setAntecedents('Arturo est un ancien chien de cirque. ');
-            $image = new Image();
-            $image->setUrlImage('img/Arturo/arthuro.jpg');
-            $dog->addImage($image);
+        //     $dog = new Dog();
+        //     $dog->setNom('Arturo');
+        //     $dog->setIfLof(1);
+        //     $dog->setAntecedents('Arturo est un ancien chien de cirque. ');
+        //     $image = new Image();
+        //     $image->setUrlImage('img/Arturo/arthuro.jpg');
+        //     $dog->addImage($image);
             
-            $dogs[] = $dog;
-        }
-        $Annonces = [];
-        for($i= 0 ; $i <1 ; $i++){
-            $annonce = new Annonce();
-            $annonce->setTitre('Arturo');
-            $annonce->setDateMAJ(new \DateTime());
-            $annonce->addDog($dogs[$i]);
-            $Annonces[] = $annonce;
-        }
+        //     $dogs[] = $dog;
+        // }
+        // $Annonces = [];
+        // for($i= 0 ; $i <1 ; $i++){
+        //     $annonce = new Annonce();
+        //     $annonce->setTitre('Arturo');
+        //     $annonce->setDateMAJ(new \DateTime());
+        //     $annonce->addDog($dogs[$i]);
+        //     $Annonces[] = $annonce;
+        // }
 
         return $this->render('detail_annonce/index.html.twig', [
-            'controller_name' => 'DetailAnnonceController',
             // 'title' => 'Dog Corner',
-            'annonces' => $Annonces,
+            'annonce' => $annonce,
         ]);
     }
 }
